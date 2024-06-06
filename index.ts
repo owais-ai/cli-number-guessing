74fc09d9832f649769f4a809e8b8edf8f2fc642c
Owais Khan <160103547+owais-ai@users.noreply.github.com>
2024-06-06 17:22:48 +0500

    Add files via upload

---
+++ index.ts
@@ -0,0 +1,27 @@
+#! /usr/bin/env node
+
+import inquirer from "inquirer";
+
+// 1) Generate a randomo number
+// 2) Input a number from user
+// 3) Compare both numbers and give the results
+
+// const randomNumber = 13;
+
+
+const randomNumber = Math.floor(Math.random() * 10);
+
+const answer = await inquirer.prompt([
+    {
+        name: "guessedNumber",
+        type: "number",
+        message: "Please guess a number",      
+    }
+])
+
+if (randomNumber == answer.guessedNumber){
+    console.log("Congratulations! The guessed number is correct");
+} else {
+    console.log("The guessed number is wrong");
+    console.log("The correct number is" +" "+ randomNumber);
+}
